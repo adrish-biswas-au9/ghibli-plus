0a1e983083947ce9fabe14e45eac3d0fcf58becb
kishan-das-au9 <kishandas909@gmail.com>
2021-01-09 21:10:56 +0530

    bugs removed

--- ghibli-universe/src/pages/ErrorPage/ErrorPage.js
+++ ghibli-universe/src/pages/ErrorPage/ErrorPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ErrorPage = (props) =>{
   return(
-    <div style={{margin: '100px 0px 0px 10px'}}>
+    <div style={{margin: '110px 0px 303px 10px'}}>
       <h2>ERROR 404</h2>
       <p><span style={{color: 'blue'}} onClick={() => props.history.push('/')} >click here</span> to go back to login page</p>
     </div>
