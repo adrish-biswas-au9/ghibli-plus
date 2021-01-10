c5cdd35375b59ee210c8d2f97954fc71e771ebd4
kishan-das-au9 <kishandas909@gmail.com>
2021-01-11 00:09:27 +0530

    few login, logout and db.json changes

--- ghibli-universe/src/pages/Home.js
+++ ghibli-universe/src/pages/Home.js
@@ -1,32 +1,10 @@
 import React,{Component} from 'react';
-
-const url = "http://localhost:5000/api/auth/userinfo";
-
-class Home extends Component{
-    constructor(){
-        super()
-
-        this.state={
-            user:'',
-            error:''
-        }
-    }
-
-    handleLogout=() => {
-        sessionStorage.removeItem('_ltk');
-        sessionStorage.removeItem('_rtk');
-        this.props.history.push('/LoginComponent')
-    }
-
-    render(){
-        if(sessionStorage.getItem('_ltk') == null){
-            this.props.history.push('/LoginComponent')
-        }
-  return(
-<div>
-
-    <button className="btn btn-danger" onClick={this.handleLogout}> Logout </button>
-</div>
- )}}
+import Logout from './Logout';
+const Home = (props) =>{
+  return(<div>
+    <Logout history={props.history}/>
+  </div>)
+  
+}
 
 export default Home;
